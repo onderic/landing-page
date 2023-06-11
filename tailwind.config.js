19385431d4b52994900c32ff680c400b996f5b5b
onderi <ondericollins80@gmail.com>
2023-06-11 18:34:18 +0300

    update main section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'bg-card': '#deb887',
+      },
+      backgroundImage: {
+        'card-pattern': "url('./assets/architecture-g0d51376cf_640.jpg')",
+      }
+
+    },
   },
   plugins: [],
 }
